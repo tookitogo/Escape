4fbe50a8e21b91b286e9bce781bdc3b5a3738ac7
tookitogo <tooki@usa.net>
2021-01-04 11:29:37 +0100

    Update Escape.cpp

--- Escape/Escape.cpp
+++ Escape/Escape.cpp
@@ -1,9 +1,9 @@
 //---------------------------------------------------------------- 
-// Titel:       Konsolen-Programm Escape 1-3
-// Projekt:     Escape.cpp
-// Datum:       2021-01-04 AT 
-// Funktion:    Übungsaufgabe Escape-Sequenzen
-//               
+// Titel:		Konsolen-Programm Escape 1-3
+// Projekt:		Escape.cpp
+// Datum:		2021-01-04 AT 
+// Funktion:	Übungsaufgabe Escape-Sequenzen
+//				 
 //---------------------------------------------------------------- 
 
 
@@ -12,18 +12,18 @@
 
 void main(void)
 {
-    // ESCAPE1
-    printf("Antonio Tejada\nEngelstrasse 9\n8004 Z\x81rich");                 //Textausgabe 
-    printf("\n");                                                             //Textausgabe 
-    printf("\n");                                                             //Textausgabe 
+	// ESCAPE1
+	printf("Antonio Tejada\nEngelstrasse 9\n8004 Z\x81rich");				  //Textausgabe 
+	printf("\n");															  //Textausgabe 
+	printf("\n");															  //Textausgabe 
 
 // ESCAPE2
-    printf("Antonio Tejada\nEngelstrasse 9\n8004 Z\x81rich\a");               //Textausgabe
-    printf("\n");                                                             //Textausgabe 
-    printf("\n");                                                             //Textausgabe 
+	printf("Antonio Tejada\nEngelstrasse 9\n8004 Z\x81rich\a");				  //Textausgabe
+	printf("\n");															  //Textausgabe 
+	printf("\n");															  //Textausgabe 
 
  // ESCAPE3
-    printf("\xE1");                                                           //Textausgabe 
-    printf("\n");                                                             //Textausgabe 
+	printf("\xE1");															  //Textausgabe 
+	printf("\n");															  //Textausgabe 
 
-}  
+}
